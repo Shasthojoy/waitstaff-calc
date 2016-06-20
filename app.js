248e6c54b4c4f7307b1a7ec290c82980223cab4a
Joe Reed <joe.reed.4@gmail.com>
2016-06-20 22:51:19 +0100

    Adds reset functions

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ angular.module('waiterCalc', [])
         var vm = this;
 
         vm.currentMeal = {
-            mealPrice: 0,
-            taxRate: 0,
-            tipRate: 0,
+            mealPrice: '',
+            taxRate: 15,
+            tipRate: 10,
             tax: 0,
             subtotal: 0,
             tip: 0,
             total: 0,
             calcSubtotal: function(){
-                this.tax = Math.round(this.mealPrice * this.taxRate * 100)/100;
+                this.tax = Math.round(this.mealPrice * this.taxRate)/100;
                 this.subtotal = parseFloat(this.mealPrice) + this.tax;
             },
             calcTip: function(){
-                this.tip = Math.round(this.subtotal * this.tipRate * 100)/100;
+                this.tip = Math.round(this.subtotal * this.tipRate)/100;
             },
             calcTotal: function(){
                 this.total = this.subtotal + this.tip;
@@ -31,19 +31,36 @@ angular.module('waiterCalc', [])
             }
         };
 
-        vm.submit = function(userInput){
-            vm.currentMeal.mealPrice = parseFloat(userInput.mealPrice.$modelValue);
-            vm.currentMeal.taxRate = parseFloat(userInput.taxRate.$modelValue)/100;
-            vm.currentMeal.tipRate = parseFloat(userInput.tipRate.$modelValue)/100;
+
+        vm.reset = function(){
+            vm.currentMeal.mealPrice = '';
+        }
+
+        vm.submit = function(){
+            // Run currentMeal methods
             vm.currentMeal.calcSubtotal();
             vm.currentMeal.calcTip();
             vm.currentMeal.calcTotal();
 
+            // Update myEarnings object
             vm.myEarnings.mealCount++;
             vm.myEarnings.tipTotal += vm.currentMeal.tip;
             vm.myEarnings.calcAvg();
 
-            console.log(vm.currentMeal);
-            console.log(vm.myEarnings);
+            // Reset Form
+            vm.reset();
         }
+
+        vm.totalReset = function(){
+            vm.myEarnings.averageTip = 0;
+            vm.myEarnings.mealCount = 0;
+            vm.myEarnings.tipTotal= 0;
+
+            vm.currentMeal.mealPrice = '';
+            vm.currentMeal.subtotal = 0;
+            vm.currentMeal.tax  = 0;
+            vm.currentMeal.tip = 0;
+            vm.currentMeal.total = 0;
+        }
+
     })
